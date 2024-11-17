6cf9628f9668381465c5689616c448a0427ebb18
swagata12 <kunal712701@gmail.com>
2024-11-17 20:23:59 +0530

    oddnum first commit

--- test1.js
+++ test1.js
@@ -1,1 +1,14 @@
-// Test js file from Swagata Dhara
+let num = [2, 5, 7, 6, 18, 19, 9, 20, 21];
+function findOddnumber(n) {
+  let countOdd = 0;
+  let countEven = 0;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] % 2 === 0) {
+      countEven++;
+    } else {
+      countOdd++;
+    }
+  }
+  return [countOdd, countEven];
+}
+console.log(findOddnumber(num));
